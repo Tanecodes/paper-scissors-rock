8a711e924d44213a50aaa8ce23d7b4e6406c1e89
Tanecodes <tanecodes@gmail.com>
2025-02-14 10:51:35 +1000

    so far ive implemented call back functions for each game button and returns a random computer result

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
-const rockBtn = document.getElementById("rock");
-const paperBtn = document.getElementById("paper");
-const scissorsBtn = document.getElementById("scissors");
+const rockBtn = document.getElementById("rock").addEventListener("click",function(){
+  playerResult.textContent = "player: ROCK!";
+  computerResult.textContent = gameLoop[Math.floor(Math.random()*3)];
+});
+const paperBtn = document.getElementById("paper").addEventListener("click",function(){
+  playerResult.textContent = "player: PAPER!"
+  computerResult.textContent = gameLoop[Math.floor(Math.random()*3)];
+});
+const scissorsBtn = document.getElementById("scissors").addEventListener("click",function(){
+  playerResult.textContent = "player: SCISSORS!";
+  computerResult.textContent = gameLoop[Math.floor(Math.random()*3)];
+});
+
+let gameLoop = ["computer: ROCK!","computer:PAPER!","computer:SCISSORS!"];
+let playerResult = document.getElementById("playerResult");
+let computerResult = document.getElementById("computerResult");
+let result = document.getElementById("result");
+
